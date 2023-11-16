ed4e70e593db2cdd3f37f720ba8770667afaa74e
Guillermo Boggiano <alejandro0887@outlook.com>
2023-11-16 14:26:25 -0300

    final corrections

--- src/components/CartWidgetShop_comp/CartWidgetSh.jsx
+++ src/components/CartWidgetShop_comp/CartWidgetSh.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import cartshop from "../../Assets//shoppingcart.png";
 import { useId } from "react";
 import "./style.css";
 import { useCart } from "../CustomHook-comp/useCart";
 import { useContext } from "react";
 import { Cartcontext } from "../../context/CartContext";
+import { collection, getDocs, where, query, addDoc } from "firebase/firestore";
+import { db } from "../..";
+import Swal from "sweetalert2";
 
 function CartItem({
   title,
@@ -31,9 +34,55 @@ function CartItem({
   );
 }
 
+//-------------------------------------------------------------
+
 export default function CartWidgetSh() {
   const cartCheckboxId = useId();
   const { cart, clearCart, addToCart } = useContext(Cartcontext);
+  const [newUser, setNewUser] = useState();
+  const [showForm, setShowForm] = useState(true);
+  const [docIds, setDocIds] = useState();
+
+  const getTotal = (products) => {
+    return products.reduce((sum, product) => {
+      return sum + product.price * product.quantity;
+    }, 0);
+  };
+
+  const total = getTotal(cart);
+
+  //-----
+
+  const finish = () => {
+    const usersCollection = collection(db, "usuarios");
+    addDoc(usersCollection, newUser)
+      .then((docRef) => {
+        const docId = docRef.id;
+        console.log(docId);
+        setDocIds(docId);
+        Swal.fire(
+          "Thanks so much for your order! your tracking number is: " +
+            docId +
+            " total USD$ : " +
+            total
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setShowForm(false);
+  };
+
+  //------
+
+  const handleChange = (e) => {
+    let obj = { name: "test1", tel: 0, mail: "example@test.com" };
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+    console.log();
+    console.log({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  //-------
 
   return (
     <>
@@ -51,18 +100,35 @@ export default function CartWidgetSh() {
               {...product}
             />
           ))}
-
-          <button onClick={clearCart}>Clear</button>
+          <p>{total} $USD</p>
+          <button className="btn btn-dark" onClick={clearCart}>
+            Clear
+          </button>
+          {showForm && (
+            <>
+              <label>Name</label>
+              <input
+                type="text"
+                name="name"
+                onChange={(e) => handleChange(e)}
+              ></input>
+              <label>Tel</label>
+              <input
+                type="text"
+                name="tel"
+                onChange={(e) => handleChange(e)}
+              ></input>
+              <label>Mail</label>
+              <input
+                type="text"
+                name="mail"
+                onChange={(e) => handleChange(e)}
+              ></input>
+              <input type="submit" onClick={finish} />
+            </>
+          )}
         </ul>
       </aside>
     </>
   );
 }
-
-//  {cart.map((product) => (
-//     <CartItem
-//       key={product.id}
-//       addToCart={() => addToCart(product)}
-//       {...product}
-//     />
-//   ))}
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -27,6 +27,7 @@ export function CartProvider({ children }) {
   const clearCart = () => {
     setCart([]);
   };
+
   return (
     <Cartcontext.Provider value={{ cart, addToCart, clearCart }}>
       {children}
